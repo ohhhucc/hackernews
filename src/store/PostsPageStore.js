77a49d4263b28e3d57bb0cf386de47a552cf106b
ohhhucc <76487504+ohhhucc@users.noreply.github.com>
2021-12-06 14:02:31 +0300

    refactor posts fetching function to optimize the process

--- src/store/PostsPageStore.js
+++ src/store/PostsPageStore.js
@@ -6,6 +6,7 @@ class PostsPageStore {
         makeAutoObservable(this);
     }
 
+    ids = [];
     posts = [];
     isLoading = false;
 
@@ -15,12 +16,27 @@ class PostsPageStore {
             const idsResponse = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty&orderBy=%22$key%22&limitToFirst=100');
             const ids = await idsResponse.json();
             const posts = [];
-            for (let i = 0; i < ids.length; i++) {
-                const dataResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${ids[i]}.json?print=pretty`);
-                const data = await dataResponse.json();
-                posts.push(data);
+            if(this.ids.length !== 0) {
+                const difference = ids.filter(x => !this.ids.includes(x));
+                if(difference.length !== 0) {
+                    for (let i = 0; i < difference.length; i++) {
+                        const differenceResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${difference[i]}.json?print=pretty`)
+                        const differenceData = await differenceResponse.json();
+                        this.posts.pop();
+                        this.posts.unshift(differenceData);
+                        this.ids.pop();
+                        this.ids.unshift(difference[i]);
+                    }
+                }
+            } else {
+                for (let i = 0; i < ids.length; i++) {
+                    const dataResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${ids[i]}.json?print=pretty`);
+                    const data = await dataResponse.json();
+                    posts.push(data);
+                }
+                this.ids = ids;
+                this.posts = posts;
             }
-            this.posts = posts;
         } catch (e) {
             console.log(e);
         }
